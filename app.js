9cd3dc0e99180a8795062e5db1e05fe0f7caf63d
David Fulton <david.fulton@and.digital>
2023-04-01 15:31:43 +0100

    remove hardcoded credentials and replace through .env

--- app.js
+++ app.js
@@ -7,13 +7,9 @@ const cors = require('cors');
 const querystring = require('querystring');
 require('dotenv').config(); // to store client variables
 
-// const CLIENT_ID = process.env.CLIENT_ID;
-// const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// const REDIRECT_URI = process.env.REDIRECT_URI;
-// // credentials for test application
-const CLIENT_ID = '48b0846a1e814a1088a62061f229b189';
-const CLIENT_SECRET = 'c603f273b3944dcd930d0b6e0e2c0f54';
-const REDIRECT_URI = 'http://localhost:8888/callback';
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -31,7 +27,7 @@ const generateRandomString = function (length) {
   return text;
 };
 
-const stateKey = 'spotify_auth_state';
+const stateKey = 'spotify_auth_state'; // used to store state in cookie
 
 app.use(cors()).use(cookieParser());
 app.get('/login', function (req, res) {
@@ -52,6 +48,7 @@ app.get('/login', function (req, res) {
       })
   );
 });
+// add pkce code challenge to code
 
 app.get('/callback', function (req, res) {
   // your application requests refresh and access tokens
