32acbbc21ca65115b0e9b8a57ff0fe1c00191f43
DJMF2015 <djm.fulton@gmail.com>
2023-04-21 12:53:26 +0100

    move styles into seperate styles folder

--- client/src/components/RecommendationsCard.js
+++ client/src/components/RecommendationsCard.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import SpotifyPreview from '../utils/SpotifyPreview';
 import CreatePlaylistButton from './CreatePlaylistBtn';
+import RecommendationsGridStyles from '../styles/RecommendationsGridStyles';
 import useCreatePlaylist from '../hooks/useCreatePlaylist';
 
-const style = {
-  rangeButtons: {
-    justifyContent: 'left',
-    alignItems: 'left',
-    textAlign: 'left',
-    margin: '3em 2em 2em 2em',
-    padding: '2em 1em 2em 2em',
-    border: '2px solid red',
-    borderRadius: '2rem',
-    lineHeight: '4rem',
-  },
-};
 const RecommendationsCard = (props) => {
   const { createTopTracksPlaylist } = useCreatePlaylist(props.artistData);
 
@@ -32,89 +21,98 @@ const RecommendationsCard = (props) => {
             <h1>Recommendations</h1>
             <CreatePlaylistButton onClick={createTopTracksPlaylist} />
           </div>
-          <div style={style.rangeButtons}>
-            <input
-              type="range"
-              name="popularity"
-              min="0"
-              max="100"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Popularity </label>
+          <RecommendationsGridStyles>
+            <div className="range_button_and_preview_grid_container">
+              <div className="range_buttons">
+                <input
+                  type="range"
+                  name="popularity"
+                  min="0"
+                  max="100"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Popularity </label>
 
-            <input
-              type="range"
-              min="0"
-              name="instrumentalness"
-              max="0.9"
-              step="0.01"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Instrumentalness</label>
+                <input
+                  type="range"
+                  min="0"
+                  name="instrumentalness"
+                  max="0.9"
+                  step="0.01"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Instrumentalness</label>
 
-            <input
-              type="range"
-              min="0"
-              name="energy"
-              max="0.9"
-              step="0.01"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Energy</label>
+                <input
+                  type="range"
+                  min="0"
+                  name="energy"
+                  max="0.9"
+                  step="0.01"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Energy</label>
 
-            <input
-              type="range"
-              min="0"
-              name="danceability"
-              max="0.9"
-              step="0.01"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Danceability</label>
+                <input
+                  type="range"
+                  min="0"
+                  name="danceability"
+                  max="0.9"
+                  step="0.01"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Danceability</label>
 
-            <input
-              type="range"
-              min="0"
-              name="valence"
-              max="0.9"
-              step="0.01"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Valence</label>
+                <input
+                  type="range"
+                  min="0"
+                  name="valence"
+                  max="0.9"
+                  step="0.01"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Valence</label>
 
-            <input
-              type="range"
-              min="0"
-              name="tempo"
-              max="200"
-              step="1"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Tempo</label>
+                <input
+                  type="range"
+                  min="0"
+                  name="tempo"
+                  max="200"
+                  step="1"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Tempo</label>
 
-            <input
-              type="range"
-              min="0"
-              name="speechiness"
-              max="0.9"
-              step="0.01"
-              onChange={props.handleInputChange}
-            />
-            <label style={{ margin: '2em' }}>Speechiness</label>
-          </div>
+                <input
+                  type="range"
+                  min="0"
+                  name="speechiness"
+                  max="0.9"
+                  step="0.01"
+                  onChange={props.handleInputChange}
+                />
+                <label style={{ margin: '2em' }}>Speechiness</label>
+              </div>
+              <div>
+                {props.artistData?.tracks &&
+                  props.artistData?.tracks.map((track, i) => (
+                    <>
+                      <p> {track?.name}</p>
+                      <SpotifyPreview
+                        style={{
+                          border: '1px solid black',
+                          width: '400px',
+                          margin: '1em',
+                        }}
+                        link={track.external_urls?.spotify}
+                      />
+                    </>
+                  ))}
+              </div>
+            </div>
+          </RecommendationsGridStyles>
         </>
       )}
-
-      {props.artistData?.tracks &&
-        props.artistData?.tracks.map((track, i) => (
-          <>
-            <p> {track?.name}</p>
-            <SpotifyPreview
-              style={{ border: '1px solid black', width: '400px', margin: '1em' }}
-              link={track.external_urls?.spotify}
-            />
-          </>
-        ))}
     </div>
   );
 };
